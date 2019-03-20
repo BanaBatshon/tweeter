13449a050929c5866a6430004a4f06ba34cb9f65
Bana <banabatshon@hotmail.com>
2019-03-20 20:53:19 +0000

    creats a function that counts the number of characters left for the tweet composer. Notifies the user when they have crossed 140 and turns the counter colour to red. turns the counter colour to yello when they have reached the zero limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    let maxLength = 140;
+    $("textArea").on('keyup', function() {
+      let length = $(this).val().length;
+      let counter = maxLength - length;
+      $(".counter").html(counter);
+      if (counter < 0) {
+        $(".counter").css('color', 'red');
+      } 
+      else if (counter === 0) {
+        $(".counter").css('color', 'orange');
+      } else {
+        $(".counter").css('color', 'black');
+      }
+  })
+});
