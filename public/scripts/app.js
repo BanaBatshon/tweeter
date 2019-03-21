fe4e58d2e812b00f415c9ed8480ef7ea21ddd322
Bana <banabatshon@hotmail.com>
2019-03-21 04:30:49 +0000

    creats a post and get method functions to attain data from the server and manipulate it to display the tweet on the webpage without refreshing. Displays alert msgs for certain conditions of the tweet length

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,49 +1,3 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 $(document).ready(function () {
 
@@ -79,7 +33,7 @@ $(document).ready(function () {
     h6.append(handle);
     tweet.append(div);
     div.append(p);
-    p.append(content);
+    p.append($("<p></p>").text(content)); // prevents XSS with escaping
     tweet.append(footer);
     footer.append(time);
     footer.append(span);
@@ -92,14 +46,39 @@ $(document).ready(function () {
 
 
 function renderTweets(tweets) {
-  // loops through tweets
   for ( const tweet of tweets) {
     let newTweet = createTweetElement(tweet);
-    $('#all-tweets').append(newTweet);
+    $('#all-tweets').prepend(newTweet);
   }
 }
 
-renderTweets(data);
+function loadTweets (){
+  $.ajax({url: '/tweets',method: 'GET',success: function(data){
+      renderTweets(data);
+    },
+    error: function(err){
+      console.log("there was an error getting data");
+    }
+  });
+}
+
+let $form = $('#newTweetForm');
+$form.submit(function (event) {
+  event.preventDefault();
+  if ($("textArea").val().length === 0){
+    alert('Please type something to tweet');
+  } else if ($("textArea").val().length > 140 ) {
+    alert('The length of your tweet exceeds the character limit')
+  } else {
+    let $formInput = $(this).serialize();
+    $.ajax({ method: 'POST', url: "/tweets", data: $formInput})
+    .done(function () {
+      loadTweets();
+    });
+  }
+})
+
+
 
 })
 
